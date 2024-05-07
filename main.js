982fdb50738167c39a1d783543a36a10d49f7205
Nicolette Reneilwe Mashaba <122819051+Reney17@users.noreply.github.com>
2024-05-07 12:11:54 +0200

    function for options

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert("js");
+//Default Variables
+
+let billAmount = 0.0;
+let tipPercent = 0;
+let numPeople = 1;
+let tipAmountPerPerson = 0;
+let totalPerPerson = 0;
+
+// Calculate Tip Amount
+
+function calculateTipAmount() {
+  // Bill
+  billAmount = document.getElementById("bill_amount").value;
+}
+// Add EventListeners to Options
+const tipOptions = document.getElementsByClassName("option");
+for (let index = 0; index < tipOptions.length; index++) {
+  const option = tipOptions[index];
+
+  option.addEventListener("click", () => {
+    alert("option.textContent");
+  });
+}
